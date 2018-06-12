740a6bfb365fff5e8e964fffdc1d7c5defef85f5
Ryand9010 <ryandavis0@gmail.com>
2018-06-12 15:42:32 -0700

    Player Movement Added

--- Infinite_Runner/Assets/Scripts/PlayerScript.cs
+++ Infinite_Runner/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     public float jumpPower = 3.0f;
+   
+    [SerializeField]
+    private float movementSpeed;
     Rigidbody2D myRigidBody;
     bool isGrounded = false;
     float posX = 0.0f;
@@ -22,18 +25,35 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKey(KeyCode.Space) && isGrounded && !isGameOver)
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+        PlayerJump();
+
+        //Hit Check
+        //if (transform.position.x < posX)
+        //{
+        //    GameOver();
+        //}
+	}
+
+
+    private void HandleMovement(float horizontal)
+    {
+
+        myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+        
+    }
+
+    private void PlayerJump()
+    {
+        //Jump
+        if (Input.GetKey(KeyCode.Space) && isGrounded && !isGameOver)
         {
             myRigidBody.AddForce(Vector3.up * (jumpPower * myRigidBody.mass * myRigidBody.gravityScale * 15));
         }
-
-        //Hit Check
-        if (transform.position.x < posX)
-        {
-            GameOver();
-        }
-	}
-
+    }
 
     private void Update()
     {
