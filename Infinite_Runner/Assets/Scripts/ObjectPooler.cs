1b13217f7781679fc7e8302a6fa482baf7cad20d
jensineramirez <jensineramirez@users.noreply.github.com>
2018-06-12 15:16:11 -0700

    Cleaned up/Fixed settings

---
+++ Infinite_Runner/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool p in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for(int i = 0; i < p.size; i++)
+            {
+                GameObject obj = Instantiate(p.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(p.tag, objectPool);
+        }
+	}
+
+    public GameObject SpawnFromPool(string tag, Vector2 position)
+    {
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            print("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
