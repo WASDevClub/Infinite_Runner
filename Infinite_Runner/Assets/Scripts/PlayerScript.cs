7019d7efff72d1ab9d629e268511209312faeba9
jensineramirez <jensineramirez@users.noreply.github.com>
2018-06-12 18:15:34 -0700

    Reverted to Test-Branch Commit - Works

--- Infinite_Runner/Assets/Scripts/PlayerScript.cs
+++ Infinite_Runner/Assets/Scripts/PlayerScript.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     public float jumpPower = 3.0f;
-   
-    [SerializeField]
-    private float movementSpeed;
     Rigidbody2D myRigidBody;
     bool isGrounded = false;
     float posX = 0.0f;
     bool isGameOver = false;
-    
+    ChallengeController myChallengeController;
     
 
 
@@ -20,43 +17,36 @@ public class PlayerScript : MonoBehaviour {
 	void Start () {
         myRigidBody = transform.GetComponent<Rigidbody2D>();
         posX = transform.position.x;
-       
+        myChallengeController = GameObject.FindObjectOfType<ChallengeController>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-
-        float horizontal = Input.GetAxis("Horizontal");
-
-        HandleMovement(horizontal);
-        PlayerJump();
-
-        
-	}
-
-
-    private void HandleMovement(float horizontal)
-    {
-
-        myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
-        
-    }
-
-    private void PlayerJump()
-    {
-        //Jump
-        if (Input.GetKey(KeyCode.Space) && isGrounded && !isGameOver)
+		if (Input.GetKey(KeyCode.Space) && isGrounded && !isGameOver)
         {
             myRigidBody.AddForce(Vector3.up * (jumpPower * myRigidBody.mass * myRigidBody.gravityScale * 15));
         }
-    }
+
+        //Hit Check
+        if (transform.position.x < posX)
+        {
+            GameOver();
+        }
+	}
+
 
     private void Update()
     {
         
     }
 
-    
+    void GameOver()
+    {
+        isGameOver = true;
+        myChallengeController.GameOver();
+
+        FindObjectOfType<GameManager>().EndGame();
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
