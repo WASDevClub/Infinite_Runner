9d52a6b2fa4edf830a576b36b6f53d09cdd998b6
jensineramirez <jensineramirez@users.noreply.github.com>
2018-06-12 20:07:14 -0700

    Added more to the platforms

---
+++ Infinite_Runner/Assets/Scripts/CeilingGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingGenerator : MonoBehaviour {
+
+    public GameObject ceiling;
+    public Transform generationPoint;
+    private float ceilingWidth;
+
+    // Use this for initialization
+    void Start(){
+        ceilingWidth = ceiling.GetComponent<BoxCollider2D>().size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + ceilingWidth, transform.position.y, transform.position.z);
+            Instantiate(ceiling, transform.position, transform.rotation);
+        }
+    }
+}
