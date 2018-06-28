a45219d7dd1c2f7256c1de4d20ea39fb4e94e27b
jensineramirez <jensineramirez@users.noreply.github.com>
2018-06-28 11:25:55 -0700

    Added new platform sizes - put in array. Add new Obstacles and randomized placements.

---
+++ Infinite_Runner/Assets/Scripts/ObjectPoolerObstacles.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolerObstacles : MonoBehaviour {
+
+    public GameObject stalagmite;
+    public int pooledStalagmiteAmount;
+    List<GameObject> poolStalagmite;
+
+    // Use this for initialization
+    void Start()
+    {    
+        poolStalagmite = new List<GameObject>();
+        for (int i = 0; i < pooledStalagmiteAmount; i++)
+        {
+            GameObject objStalagmite = (GameObject)Instantiate(stalagmite);
+            objStalagmite.SetActive(false);
+            poolStalagmite.Add(objStalagmite);
+        }
+    }
+    public GameObject getPooledStalagmite()
+    {
+        for(int i = 0; i < pooledStalagmiteAmount; i++)
+        {
+            if (!poolStalagmite[i].activeInHierarchy)
+                return poolStalagmite[i];
+        }
+
+        GameObject obj = (GameObject)Instantiate(stalagmite);
+        obj.SetActive(false);
+        poolStalagmite.Add(obj);
+
+        return obj;
+    }
+}
--- Infinite_Runner/Assets/Scripts/PlatformGenerator.cs
+++ Infinite_Runner/Assets/Scripts/PlatformGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour {
 
+    #region Platforms
     public GameObject platform; //GETTING THE PLATFORM (FLOOR) OBJECT
     public Transform generationPoint; //POINT IN WHICH THE FLOOR SHOULD GENERATE
     public float distanceBetween; //GETTING THE DISTANCE BETWEEN EACH PLATFORM
@@ -14,16 +15,42 @@ public class PlatformGenerator : MonoBehaviour {
     public float distanceBetweenMax;
 
     public ObjectPoolerFloors objFloor;  //AN OBECT FOR THE OBJECT POOL FOR FLOORS
+    #endregion
 
+    #region Berries
     public ObjectPoolerBerries objBerry; //AN OBJECT FOR THE OBJECT POOL FOR BERRIES
 
     public float berryHeightMin; //SETTING MIN & MAX HEIGHT
     public float berryHeightMax;
     public float berryHeight; //CHOSEN HEIGHT
+    #endregion
+
+    #region Obstacles
+    public ObjectPoolerObstacles objStalagmite;
+
+    public float stalagMin;
+    public float stalagMax;
+    public float stalagDistance;
+    #endregion
+
+    #region Arrays
+    public GameObject[] thePlatforms;
+    private int platformSelector;
+
+    private float[] platformWidths;
+    #endregion
+
 
     // Use this for initialization
     void Start () {
-        platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+        //platformWidth = platform.GetComponent<BoxCollider2D>().size.x;
+
+        platformWidths = new float[thePlatforms.Length];
+
+        for(int i = 0; i < thePlatforms.Length; i++)
+        {
+            platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+        }
     }
 	
 	// Update is called once per frame
@@ -32,16 +59,20 @@ public class PlatformGenerator : MonoBehaviour {
         {
             //GETTING RANDOM NUMBERS FOR THE DISTANCE
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+            //GETTING THE INDEX FOR THE PLATFORMS (WIDTHS) ARRAY
+            platformSelector = Random.Range(0, thePlatforms.Length);
             //MAKING A NEW POSITION ACCORDINGLY
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + platformWidths[platformSelector] + distanceBetween, transform.position.y, transform.position.z);           
+            
+            Instantiate(thePlatforms[platformSelector], transform.position, transform.rotation);
 
             //CALLING THE OBJECT POOL FOR PLOORS CLASS AND FINDING AN INACTIVE PLATFORM OR MAKING A NEW ONE AND RETURNING THAT GAMEOBJECT
-            GameObject newFloor = objFloor.GetPooledFloor();
+            //GameObject newFloor = objFloor.GetPooledFloor();
 
             //SETTING A POSITION AND ROTATION FOR RETURNED GAME OBJECT
-            newFloor.transform.position = transform.position;
-            newFloor.transform.rotation = transform.rotation;
-            newFloor.SetActive(true);
+            //newFloor.transform.position = transform.position;
+            //newFloor.transform.rotation = transform.rotation;
+            //newFloor.SetActive(true);
 
             //CHOOSING RANDOM RANGE FOR HEIGHT OF BERRY
             berryHeight = Random.Range(berryHeightMin, berryHeightMax);
@@ -53,6 +84,14 @@ public class PlatformGenerator : MonoBehaviour {
             newBerry.transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, berryHeight, transform.position.z);
             newBerry.transform.rotation = transform.rotation;
             newBerry.SetActive(true);
+
+            stalagDistance = Random.Range(0, platformWidth);
+
+            GameObject newStalag = objStalagmite.getPooledStalagmite();
+
+            newStalag.transform.position = new Vector3(transform.position.x + /*platformWidth +*/ stalagDistance, platform.transform.position.y + 1.80f, transform.position.z);
+            newStalag.transform.rotation = transform.rotation;
+            newStalag.SetActive(true);
         }
 	}
 }
